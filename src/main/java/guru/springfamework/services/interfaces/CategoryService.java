2c13c36c7843e4c64a9ef15fa3bee4eee9e11597
sukhrobdev <suxrobdevbk@gmail.com>
2022-11-13 15:18:47 +0500

    Category domain , services, repository and controller done!

---
+++ src/main/java/guru/springfamework/services/interfaces/CategoryService.java
@@ -0,0 +1,16 @@
+package guru.springfamework.services.interfaces;
+
+
+import guru.springfamework.domain.Category;
+
+import java.util.List;
+
+/**
+ * Created by jt on 9/26/17.
+ */
+public interface CategoryService {
+
+    List<Category> getAllCategories();
+
+    Category getCategoryByName(String name);
+}
